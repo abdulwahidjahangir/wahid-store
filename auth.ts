fb2eeefa3e6c673a8629f1028d8e1876a35b7a1d
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-02-23 19:29:59 +0100

    customized JWT token based on the requirment

--- auth.ts
+++ auth.ts
@@ -55,9 +55,12 @@ export const config = {
     }),
   ],
   callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // if there is an update set the user name
       if (trigger === "update") {
@@ -65,6 +68,30 @@ export const config = {
       }
 
       return session;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // If user has no name then use email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // update the database to reflect token name
+
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              name: token.name,
+            },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
