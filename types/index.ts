5f1fcc7f5b0887600a1532679c5b826c8b3b8663
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-02-23 23:20:49 +0100

    created schema and type for typescript for cart

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
-import { inserProductsSchema } from "@/lib/validator";
+import {
+  inserProductsSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from "@/lib/validator";
 
 export type Product = z.infer<typeof inserProductsSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+
+export type CartItem = z.infer<typeof cartItemSchema>;
