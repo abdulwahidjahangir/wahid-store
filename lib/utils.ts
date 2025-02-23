4a164b6c921a7146aeb16ba0972c234b72c8cbae
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-02-23 19:21:00 +0100

    robust error handling for sign up page

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,3 +17,29 @@ export function formatNumberWithDecimal(num: number): string {
     ? `${intValue}.${floatValue.padEnd(2, "0")}`
     : `${intValue}.00`;
 }
+
+// Format Error
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function formatError(error: any) {
+  if (error.name === "ZodError") {
+    // Handle Zod Error
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name == "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Handle Prisma Error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    // Handle Other Error
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
