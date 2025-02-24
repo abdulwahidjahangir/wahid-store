44de0ee626149827ad72f1c27e5e4110ce8468b5
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-02-25 00:26:29 +0100

    updating user experience

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,48 +1,54 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    // Handle error add to cart
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
+      // Handle error add to cart
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    // Handle success add to cart
-    toast.success(`${res.message}`, {
-      action: {
-        label: "Go to Cart",
-        onClick: () => router.push("/cart"),
-      },
+      // Handle success add to cart
+      toast.success(`${res.message}`, {
+        action: {
+          label: "Go to Cart",
+          onClick: () => router.push("/cart"),
+        },
+      });
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    // Handle error add to cart
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
+      // Handle error add to cart
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    // Handle success add to cart
-    toast.success(`${res.message}`, {
-      action: {
-        label: "Go to Cart",
-        onClick: () => router.push("/cart"),
-      },
+      // Handle success add to cart
+      toast.success(`${res.message}`, {
+        action: {
+          label: "Go to Cart",
+          onClick: () => router.push("/cart"),
+        },
+      });
     });
   };
 
@@ -52,17 +58,45 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
 
   return existsItem ? (
     <div>
-      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleRemoveFromCart}
+        disabled={isPending}
+      >
+        {isPending ? (
+          <Loader className="h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existsItem.qty}</span>
-      <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleAddToCart}
+        disabled={isPending}
+      >
+        {isPending ? (
+          <Loader className="h-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
-    <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+    <Button
+      className="w-full"
+      type="button"
+      onClick={handleAddToCart}
+      disabled={isPending}
+    >
+      {isPending ? (
+        <Loader className="h-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}{" "}
+      Add To Cart
     </Button>
   );
 };
