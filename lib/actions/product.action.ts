cf609e2c8ea4dca1e81af8c2b2eab76b09fcec47
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-03-08 14:15:04 +0100

    created admin product page

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -19,4 +19,31 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// GET all products
+export async function getAllProducts({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  query,
+  limit = PAGE_SIZE,
+  page,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCounts = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCounts / limit),
+  };
 }
