b49440198b6853011a10735ee61528ffd963535d
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2025-03-09 15:04:01 +0100

    - carousel for home page - drawer for categories

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.action";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.action";
 
 export default async function Home() {
   const latestProduct = await getLatestProducts();
 
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProduct} title="Newest Arrivals" limit={4} />
     </>
   );
---
+++ components/shared/header/category-drawer.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { getAllCategories } from "@/lib/actions/product.action";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+
+const CategoryDrawer = async () => {
+  const catrgories = await getAllCategories();
+
+  return (
+    <Drawer direction="left">
+      <DrawerTrigger asChild>
+        <Button variant="outline">
+          <MenuIcon />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="h-full max-w-sm">
+        <DrawerHeader>
+          <DrawerTitle>Select a category</DrawerTitle>
+          <div className="space-y-1 mt-4">
+            {catrgories.map((x) => (
+              <Button
+                key={x.category}
+                variant="ghost"
+                className="w-full justify-start"
+                asChild
+              >
+                <DrawerClose asChild>
+                  <Link href={`/search?category=${x.category}`}>
+                    {x.category} ({x._count})
+                  </Link>
+                </DrawerClose>
+              </Button>
+            ))}
+          </div>
+        </DrawerHeader>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CategoryDrawer;
